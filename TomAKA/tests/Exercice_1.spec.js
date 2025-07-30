37b0049b481cf877a4dc1dcf07cf771a6710502b
ToM <thomasauvray7@gmail.com>
2025-07-30 21:00:28 +0200

    aa

--- TomAKA/tests/Exercice_1.spec.js
+++ TomAKA/tests/Exercice_1.spec.js
@@ -3,4 +3,8 @@ const { test, expect } = require('@playwright/test');
 test('Exercice 1 - Ouverture du site IT Akademy', async ({ page }) => {
   // Ouvrir la page du site IT Akademy
   await page.goto('https://www.it-akademy.fr');
+  // Vérifier que le titre de la page contient "IT Akademy"
+  await expect(page).toHaveTitle(/IT-Akademy/);
+
 });
+
